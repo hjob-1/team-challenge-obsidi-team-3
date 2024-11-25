7b3c2b69d2f635ec2ca8b67aa5d8fadbaed12e54
eyob <hjob12tadele@gmail.com>
2024-11-25 21:40:00 +0300

    init: project initilization

---
+++ src/team_leetcode_challeneges/questions/Questions.java
@@ -0,0 +1,24 @@
+package team_leetcode_challeneges.questions;
+
+public class Questions {
+
+//	public double average(int[] salary) {
+//		
+//	}
+//
+//	public int[] sortedSquares(int[] nums) {
+//		
+//	}
+//
+//	public int missingNumber(int[] nums) {
+//		
+//	}
+//
+//	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//		
+//	}
+//
+//	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//		
+//	}
+}
---
+++ src/team_leetcode_challeneges/questions/main.java
@@ -0,0 +1,9 @@
+package team_leetcode_challeneges.questions;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("LeetCode team challenges");
+	}
+
+}

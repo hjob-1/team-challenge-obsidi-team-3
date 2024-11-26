31c2c6d373eefd779f4260d7c2b805634811e083
eyob <hjob12tadele@gmail.com>
2024-11-27 02:14:17 +0300

    fix: project setup modified

---
+++ src/test/java/com/bptn/leetcode_challenege_section_3_team3/AppTest.java
@@ -0,0 +1,38 @@
+package com.bptn.leetcode_challenege_section_3_team3;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}

2d57147e3577ce941bdfb9eb61f13b0e50d2ca55
eyob <hjob12tadele@gmail.com>
2024-11-27 03:16:00 +0300

    feat:leetcode challenge 1 and 2 implemented

--- src/main/java/com/bptn/leetcode_challenege_section_3_team3/App.java
+++ src/main/java/com/bptn/leetcode_challenege_section_3_team3/App.java
@@ -4,10 +4,8 @@ package com.bptn.leetcode_challenege_section_3_team3;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+	}
 }
